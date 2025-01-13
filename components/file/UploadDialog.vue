6a936e46915d15adf926b22c69b77dc3529d2101
XiaoMouz <gxiaomouz@gmail.com>
2025-01-13 13:42:34 +0800

    chore(form): file upload form

--- components/file/UploadDialog.vue
+++ components/file/UploadDialog.vue
@@ -1,2 +1,136 @@
-<script setup lang="ts"></script>
-<template> </template>
+<script setup lang="ts">
+import { useForm } from 'vee-validate'
+import { toTypedSchema } from '@vee-validate/zod'
+import * as z from 'zod'
+
+import {
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from '@/components/ui/form'
+
+const formSchema = toTypedSchema(z.object({
+    title: z.string().max(50).optional().default('Untitled-' + Date.now()),
+    description: z.string().min(2).max(200).optional(),
+    private: z.boolean().default(false),
+    password: z.string().min(6).max(50).optional(),
+}))
+
+const form = useForm({
+    validationSchema: formSchema,
+})
+
+const onSubmit = form.handleSubmit((values) => {
+    console.log('Form submitted!', values)
+})
+</script>
+<template>
+    <Dialog>
+        <DialogTrigger as-child>
+            <Button>
+                <Icon name="mdi:upload-box-outline" class="size-5" />
+                <span>Upload</span>
+            </Button>
+        </DialogTrigger>
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle>Upload a new file</DialogTitle>
+            </DialogHeader>
+            <div>
+                <form @submit="onSubmit">
+                    <div class="w-full">
+                        <div class="flex md:flex-row flex-col gap-6 w-full items-center">
+                            <div class="flex-1 h-full ">
+                                <div class="flex h-full flex-col md:flex-row gap-4 md:gap-6">
+                                    <fieldset class="grid w-full gap-6 rounded-lg border p-4">
+                                        <legend class="-ml-1 px-1 text-sm font-medium flex flex-row items-center gap-1">
+                                            <Icon name="mdi:document" /> <span>Info</span>
+                                        </legend>
+                                        <FormField v-slot="{ componentField }" name="title">
+                                            <FormItem>
+                                                <FormLabel>Title</FormLabel>
+                                                <FormControl>
+                                                    <Input type="text" :placeholder="'Untitled-' + Date.now()"
+                                                        v-bind="componentField" />
+                                                </FormControl>
+                                                <FormDescription>
+                                                    This is your file title.
+                                                </FormDescription>
+                                                <FormMessage />
+                                            </FormItem>
+                                        </FormField>
+                                        <FormField class="h-full" v-slot="{ componentField }" name="description">
+                                            <FormItem>
+                                                <FormLabel>Description</FormLabel>
+                                                <FormControl>
+                                                    <Textarea type="text" :placeholder="''" v-bind="componentField" />
+                                                </FormControl>
+                                                <FormDescription>
+                                                    This is your file description.
+                                                </FormDescription>
+                                                <FormMessage />
+                                            </FormItem>
+                                        </FormField>
+                                    </fieldset>
+
+                                </div>
+
+                            </div>
+                            <div>
+                                <div class="flex flex-col xl:flex-row gap-4 md:gap-6">
+                                    <fieldset class="grid w-full gap-6 rounded-lg border p-4">
+                                        <legend class="-ml-1 px-1 text-sm font-medium flex flex-row items-center gap-1">
+                                            <Icon name="mdi:file" /> <span>File</span>
+                                        </legend>
+                                        <Uploader />
+                                    </fieldset>
+
+                                </div>
+                                <div class="flex flex-col md:flex-row gap-4 md:gap-6">
+                                    <fieldset class="grid w-full gap-6 rounded-lg border p-4">
+                                        <legend class="-ml-1 px-1 text-sm font-medium flex flex-row items-center gap-1">
+                                            <Icon name="mdi:lock" /> <span>Privacy</span>
+                                        </legend>
+                                        <FormField v-slot="{ componentField }" name="private">
+                                            <FormItem>
+                                                <div class="flex justify-between items-center">
+                                                    <FormLabel>Private?</FormLabel>
+                                                    <FormControl>
+                                                        <Switch v-bind="componentField" />
+                                                    </FormControl>
+                                                </div>
+                                                <FormDescription>
+                                                    Only you can access this file.
+                                                </FormDescription>
+                                                <FormMessage />
+                                            </FormItem>
+                                        </FormField>
+                                        <FormField v-slot="{ componentField }" name="password">
+                                            <FormItem>
+                                                <FormLabel>Password</FormLabel>
+                                                <FormControl>
+                                                    <Input type="password" placeholder="Keep empty for public file"
+                                                        v-bind="componentField" />
+                                                </FormControl>
+                                                <FormDescription>
+                                                    Set a password to protect your file.
+                                                </FormDescription>
+                                                <FormMessage />
+                                            </FormItem>
+                                        </FormField>
+                                    </fieldset>
+                                </div>
+                            </div>
+                        </div>
+                        <Button type="submit">
+                            Submit
+                        </Button>
+                    </div>
+                </form>
+            </div>
+        </DialogContent>
+    </Dialog>
+</template>
--- pages/share/file/index.vue
+++ pages/share/file/index.vue
@@ -23,11 +23,11 @@ const {
         ? `${session.value.token}:${session.value.user.email}`
         : ' ',
     },
-    
+
   }), {
-    immediate: true,
-    lazy: true,
-  }
+  immediate: true,
+  lazy: true,
+}
 )
 
 const files = computed(() => lists.value?.files)
@@ -35,7 +35,7 @@ const files = computed(() => lists.value?.files)
 onMounted(() => {
   execute()
 })
- 
+
 const userAuthed = computed(() => {
   return session.value && !userStore.isTokenExpired()
 })
@@ -44,67 +44,45 @@ const userAuthed = computed(() => {
 <template>
   <div class="flex h-full w-full flex-col items-center mb-8">
     <div class="w-[90%] xl:w-[80%] h-full flex flex-col items-start gap-6">
-      <Title
-        title="File Share"
-        icon="mdi:cloud-print-outline"
-        description="Small file sharing, storage max 10GB for 7 days"
-      />
+      <Title title="File Share" icon="mdi:cloud-print-outline"
+        description="Small file sharing, storage max 10GB for 7 days" />
       <div class="flex flex-col gap-8 w-full">
-        <div><Uploader /></div>
+        <div>
+          <FileUploadDialog />
+        </div>
         <div class="flex flex-col gap-2">
           <div class="flex flex-row gap-4 items-center">
             <div
               class="flex px-2 py-1 rounded-md flex-row gap-2 items-center cursor-pointer hover:bg-muted transition-all bg-transparent duration-300 hover:shadow-md"
-              @click="refresh()"
-            >
-              <Icon
-                name="mdi:file-document-multiple-outline"
-                class="size-5"
-              /><span class="text-xl font-bold">Your File List</span>
+              @click="refresh()">
+              <Icon name="mdi:file-document-multiple-outline" class="size-5" /><span class="text-xl font-bold">Your File
+                List</span>
             </div>
-            <Badge
-            v-if="!userAuthed"
-              ><Icon
-                class="mr-2 size-5"
-                name="mdi:account-alert-outline"
-              /><span class="cursor-default">Anonymouse&nbsp;Mode</span>
+            <Badge v-if="!userAuthed">
+              <Icon class="mr-2 size-5" name="mdi:account-alert-outline" /><span
+                class="cursor-default">Anonymouse&nbsp;Mode</span>
             </Badge>
           </div>
-          <div
-        class="flex  items-center h-full w-full border relative rounded-lg gap-4 flex-wrap p-2"
-      >
-        <div
-          v-if="listStatus === 'pending'"
-          class="w-full h-12 flex justify-center items-center absolute"
-        >
-          <LoadingCycle />
-        </div>
-        <button
-          v-if="files?.length"
-          class="flex items-center w-full md:w-72 truncate text-nowrap overflow-hidden h-full p-1 px-2 border-2 rounded-lg justify-center gap-1 hover:bg-muted transition-all duration-100"
-          v-for="i in files"
-          :key="i.id"
-          @click="
-            () => {
-              
-              $router.push(`/share/file/${i.id}`)
-            }
-          "
-        >
-          <Icon
-          v-if="listStatus !== 'pending'"
-            name="mdi:cloud-arrow-down-outline"
-          />
-          <span class="truncate text-nowrap">{{ i.name?i.name:"<<NO NAME COPYBOARD>>" }}</span>
-        </button>
-        <div v-else>
-          <Badge class="mt-1" variant="secondary"
-            ><Icon class="mr-2 size-5" name="mdi:cloud-off-outline" /><span
-              class="cursor-default"
-              >Remote&nbsp;copyboard&nbsp;list&nbsp;is&nbsp;empty</span
-            >
-          </Badge>
-        </div>
+          <div class="flex  items-center h-full w-full border relative rounded-lg gap-4 flex-wrap p-2">
+            <div v-if="listStatus === 'pending'" class="w-full h-12 flex justify-center items-center absolute">
+              <LoadingCycle />
+            </div>
+            <button v-if="files?.length"
+              class="flex items-center w-full md:w-72 truncate text-nowrap overflow-hidden h-full p-1 px-2 border-2 rounded-lg justify-center gap-1 hover:bg-muted transition-all duration-100"
+              v-for="i in files" :key="i.id" @click="() => {
+
+                $router.push(`/share/file/${i.id}`)
+              }
+                ">
+              <Icon v-if="listStatus !== 'pending'" name="mdi:cloud-arrow-down-outline" />
+              <span class="truncate text-nowrap">{{ i.name ? i.name : "<<NO NAME FILE>>" }}</span>
+            </button>
+            <div v-else>
+              <Badge class="mt-1" variant="secondary">
+                <Icon class="mr-2 size-5" name="mdi:cloud-off-outline" /><span
+                  class="cursor-default">Remote&nbsp;copyboard&nbsp;list&nbsp;is&nbsp;empty</span>
+              </Badge>
+            </div>
           </div>
         </div>
       </div>
